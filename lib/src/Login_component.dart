e9d4e29959d40756fe61a736d9e55ebac6bb9590
salrick <18019164521@163.com>
2020-06-22 19:11:16 +0800

    login

--- lib/src/Login_component.dart
+++ lib/src/Login_component.dart
@@ -12,6 +12,7 @@ import 'route_paths.dart';
   directives: [coreDirectives, routerDirectives],
 )
 class LoginComponent implements OnInit {
+  Router _router;
   List<Hero> heroes;
 
   final WishYouService _heroService;
@@ -24,4 +25,10 @@ class LoginComponent implements OnInit {
   void ngOnInit() async {
     heroes = (await _heroService.getAll()).skip(1).take(4).toList();
   }
+
+    String _heroUrl(int id) =>
+      RoutePaths.hero.toUrl(parameters: {idParam: '$id'});
+
+    Future<NavigationResult> gotoDetail(Hero hero) =>
+      _router.navigate(_heroUrl(hero.id));
 }
